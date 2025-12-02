3a53e7030665ea4c9e3a26d72023070df9c16e5d
Vicente <semogaraujo@gmail.com>
2025-12-02 18:14:57 -0300

    commit inicial projeto mentoria java

---
+++ src/test/java/br/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mentoria.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
