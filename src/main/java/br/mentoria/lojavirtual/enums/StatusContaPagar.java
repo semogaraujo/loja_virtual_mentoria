f276706d1055968a5ab20e6ac519c8750ed8a71d
ect <ect>
2025-12-18 12:06:51 -0300

    Aula mentoria 18/12/2025

---
+++ src/main/java/br/mentoria/lojavirtual/enums/StatusContaPagar.java
@@ -0,0 +1,26 @@
+package br.mentoria.lojavirtual.enums;
+
+public enum StatusContaPagar {
+	COBRANCA("Pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada"),
+	RENEGOCIADA("Renegociada");
+	
+	private String descricao;
+
+	private StatusContaPagar(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return this.descricao;
+	}
+	
+}
